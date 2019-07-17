4de3975ba4de052c71f4d84b68858335b1a82461
Thomas Taylor <thomas.taylor@kineo.com>
2019-07-17 19:30:28 +0100

    Update config

--- conf/config.schema.js
+++ conf/config.schema.js
@@ -1,7 +1,9 @@
 module.exports = {
   definition: {
     acceptedTypes: {
-      type: 'Array'
+      type: 'Array',
+      default: ['application/json'],
+      description: 'Content types the API accepts (may use MIME types or extension names)'
     }
   }
 };
