bb32bb2998f654e6146f4b96f58f0dbbf9d5d83c
Thomas Taylor <thomas.taylor@kineo.com>
2020-01-09 17:33:13 +0000

    Convert config schema to json-schema format

--- conf/config.schema.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-  definition: {
-    acceptedTypes: {
-      type: 'Array',
-      default: ['application/json'],
-      description: 'Content types the API accepts (may use MIME types or extension names)'
-    }
-  }
-};
